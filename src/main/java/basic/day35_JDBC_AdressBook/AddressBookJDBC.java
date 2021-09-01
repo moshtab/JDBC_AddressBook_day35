48ab41737c57fd1d7f9bcc979326730b7f011c44
moshtab <mdmohsin41439@gmail.com>
2021-09-01 17:29:27 +0530

    Dislaying all data of addressbook

--- src/main/java/basic/day35_JDBC_AdressBook/AddressBookJDBC.java
+++ src/main/java/basic/day35_JDBC_AdressBook/AddressBookJDBC.java
@@ -1,9 +1,81 @@
 package basic.day35_JDBC_AdressBook;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class AddressBookJDBC {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Adressbook JDBC connectivity program");
+		connectionEshtablished();
+		readAdressbook();
+	}
+
+	private static Connection getSqlConnection() {
+		Connection conn = null;
+		String dbHostUrl = "jdbc:mysql://localhost:3306/adress_book_service";
+		String userName = "root";
+		String password = "Md78678678.";
+		try {
+			conn = DriverManager.getConnection(dbHostUrl, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+
+	}
+
+	private static void connectionEshtablished() {
+		Connection conn = getSqlConnection();
+		if (conn != null) {
+			System.out.println("Connection is eshtablished");
+		}
+	}
+
+	private static void readAdressbook() {
+		System.out.println("Displaying all data of adressBook table");
+		Connection conn = getSqlConnection();
+
+		try {
+			if (conn != null) {
+				String readEmpPayroll = "SELECT * FROM adressbook";
+
+				Statement statement = conn.createStatement();
+				ResultSet resultSet = statement.executeQuery(readEmpPayroll);
+				while (resultSet.next()) {
+					String firstName = resultSet.getString(1);
+					String lastName = resultSet.getString(2);
+					String adress = resultSet.getString(3);
+					String city = resultSet.getString(4);
+					String state = resultSet.getString(5);
+					Integer zip = resultSet.getInt(6);
+					String phoneNumber = resultSet.getString(7);
+					String email = resultSet.getString(8);
+					String family = resultSet.getString(9);
+					String friends = resultSet.getString(10);
+					String professions = resultSet.getString(11);
+					String brothers = resultSet.getString(12);
+					String row = String.format(
+							"User record:\n FirstName: %s,\n LastName: %s, \n Address: %s,  \n City: %s,\n State: %s , \n Zip: %d, \n PhoneNumber: %s, \n email:%s,\n Family:%s,\n Friends:%s,\n Professions:%s,\n Brothers:%s,",
+							firstName, lastName, adress, city, state, zip, phoneNumber, email, family,friends,professions,brothers);
+					System.out.println(row);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException sqlException) {
+					System.out.println(sqlException.getMessage());
+
+				}
+			}
+		}
 
 	}
 
