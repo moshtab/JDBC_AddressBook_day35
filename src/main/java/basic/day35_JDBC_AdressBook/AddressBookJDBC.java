cb86761846995740a14d5c255c90a3fc497e57dd
moshtab <mdmohsin41439@gmail.com>
2021-09-01 15:50:31 +0530

    added msg

---
+++ src/main/java/basic/day35_JDBC_AdressBook/AddressBookJDBC.java
@@ -0,0 +1,10 @@
+package basic.day35_JDBC_AdressBook;
+
+public class AddressBookJDBC {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Adressbook JDBC connectivity program");
+
+	}
+
+}
